77350519eef5985693aa43ad110517bb7bdf2708
Jaime Kvaternik <jaime.kvat@gmail.com>
2024-02-24 08:20:37 -0500

    add more styling

--- app/components/ResultCard.tsx
+++ app/components/ResultCard.tsx
@@ -1,23 +1,17 @@
+import NumberIcon from "./NumberIcon";
+
 export default function ResultCard(props: {
-    key: number, 
+    index: number, 
     guess: string,
     list: string[],
 }): JSX.Element {
-    const basicColorRainbow = ['red', 'orange', 'yellow', 'green', 'blue']
-    const getColor = () => {
-        return props.list.indexOf(props.guess) !== -1 ? basicColorRainbow[props.list.indexOf(props.guess)] : 'gray';
-    }
-    const getNumberOrNull = (guess: string): string => {
-        const index = props.list.indexOf(guess);
-        if (index === -1) {
-            return 'X';
-        }
-        return (index + 1).toString();
+    const getNumber = (guess: string): number => {
+        return props.list.indexOf(guess);
     }
         
     return (
-        <div key={props.key} className={`flex flex-row gap-4 text-${getColor()}-500`}>
-            <div>{getNumberOrNull(props.guess)}</div>
+        <div key={props.index} className={`flex flex-row gap-4 items-center`}>
+            <NumberIcon number={getNumber(props.guess)} />
             <p>{props.guess}</p>
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,14 @@
 import { useState } from "react";
 import ResultCard from "./components/ResultCard";
 
+/* TODO 
+- implement game over overlay/sharing
+- add helper text for user
+- add more questions
+- add hints? could either let users guess w/ lives, grey if wrong or 
+  let them guess the top 5, allow hints, show board after completion, grey if used hint 
+*/
+
 export default function Home() {
   const list = ['India', 'China', 'USA', 'Indonesia', 'Pakistan']
   const question = 'largest countries in the world';
@@ -20,16 +28,10 @@ export default function Home() {
     }
   }
 
-  const resultView = guesses.map((guess, index) => {
-    if (list.includes(guess)) {
-      return (<ResultCard key={index} guess={guess} list={list}/>)
-    } else {
-      return (<ResultCard key={index} guess={guess} list={list}/>)
-    }
-  }).reverse();
+  const resultView = guesses.map((guess, index) => (<ResultCard key={index} index={index} guess={guess} list={list}/>)).reverse();
 
   return (
-    <main style={{margin: '5vh auto', width: '80%', height: '100vh'}}>
+    <main style={{margin: '5vh auto', width: '75%', height: '100vh'}}>
       <section className="flex flex-row gap-4 items-end">
         <div className="flex flex-col items-center" >
           <h1>top</h1>
@@ -41,13 +43,15 @@ export default function Home() {
         <input 
         type="text"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg p-2 w-full mt-4"
-        value={guess} 
+        value={guess}
+        placeholder="Enter your guess here..." 
+        disabled={isGameOver}
         onChange={(e) => setGuess(e.target.value)} 
         onKeyDown={(e) => e.key == 'Enter' ? handleGuess() : null} 
         />
       </section>
       <br></br>
-      <section>
+      <section className="flex flex-col gap-4">
         {resultView}
       </section>
     </main>
