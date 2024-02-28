88e5833298b9531cc1b1eca9a0d212ac9a90051b
Jaime Kvaternik <jaime.kvat@gmail.com>
2024-02-27 23:26:04 -0500

    remove unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Montserrat, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ 
