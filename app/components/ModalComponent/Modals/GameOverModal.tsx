376ffd1feb40498105f6f7bd74ea7877cdcf9df4
Jaime Kvaternik <34807814+jkvaternik@users.noreply.github.com>
2024-08-25 15:41:19 -0700

    streaks! (#55)

--- app/components/ModalComponent/Modals/GameOverModal.tsx
+++ app/components/ModalComponent/Modals/GameOverModal.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { Montserrat } from "next/font/google";
 import { ModalComponent } from "../ModalComponent";
 import { Puzzle } from '@/app/hooks/useDailyPuzzle';
-import { getShareableEmojiScore, getScoreMessage } from '@/app/utils';
+import { getShareableEmojiScore, getScoreMessage, getLocalStorageOrDefault } from '@/app/utils';
 import { ShareIcon } from "@heroicons/react/24/outline";
 
 type Props = {
@@ -20,7 +20,7 @@ const montserrat = Montserrat({
 });
 
 const GameOverModal = ({ puzzle, score, isOpen, onClose }: Props) => {
-
+  const streak = getLocalStorageOrDefault('streak', 0);
   const createShareMessage = (includeUrl: boolean) => {
     const message = {
       title: `Top 5 #${puzzle.num}`,
@@ -73,15 +73,24 @@ const GameOverModal = ({ puzzle, score, isOpen, onClose }: Props) => {
     <>
       <ToastContainer closeButton={false} />
       <ModalComponent delayMs={750} show={isOpen} onClose={onClose} showChildren={isOpen}>
-        <div className="flex flex-col p-12 pt-9 text-center items-center justify-center">
+        <div className="flex flex-col p-12 pt-9 text-center justify-center">
           <h2 className={`text-2xl mb-8 font-bold ${montserrat.className}`}>{getScoreMessage(score)}</h2>
-          <p className="mb-2 font-semibold">Top 5 #{puzzle.num}</p>
-          <p className="mb-12 text-3xl">{getShareableEmojiScore(score)}</p>
-          <button className="py-3 px-12 bg-[#304d6d] dark:bg-[#4F6479] text-white font-medium rounded-full hover:bg-[#82A0BC] mb-6" onClick={copyScore} style={{ 'transition': '0.3s' }}>
+          <p className="mb-2 left-align font-semibold">Top 5 #{puzzle.num}</p>
+          <p className="mb-4 text-3xl">{getShareableEmojiScore(score)}</p>
+          <button className="py-3 px-12 bg-[#304d6d] dark:bg-[#4F6479] text-white font-medium rounded-full hover:bg-[#82A0BC] mb-4" onClick={copyScore} style={{ 'transition': '0.3s' }}>
             <div className="flex flex-row justify-center gap-2">
-              Share score <ShareIcon className="h-6 w-6" style={{ display: 'inline-block' }} />
+              Share <ShareIcon className="h-6 w-6" style={{ display: 'inline-block' }} />
             </div>
           </button>
+          <div className="relative flex py-3 items-center">
+            <div className="flex-grow border-t border-gray-500 dark:border-gray-400"></div>
+            <span className="flex-shrink mx-4 text-gray-500 dark:text-gray-400">Statistics</span>
+            <div className="flex-grow border-t border-gray-500 dark:border-gray-400"></div>
+          </div>
+          <div className='w-full flex justify-between mb-2'>
+            <p className="mb-2">Streak</p>
+            <p className="mb-2">{streak === 0 ? `${streak} 😔` : `${streak} 🔥`}</p>
+          </div>
           {puzzle.url != null ? <a href={puzzle.url} className={`underline text-sm text-[#304d6d] dark:text-white hover:text-[#82A0BC] active:text-[#38405F]`} target="_blank">Quiz Source</a> : null}
         </div>
       </ModalComponent>
