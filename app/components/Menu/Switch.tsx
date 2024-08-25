e3b534c7e2ebd03112e2bc82a932bc09956ef014
Jaime Kvaternik <jaime.kvat@gmail.com>
2024-08-25 19:09:01 -0400

    add switch file

---
+++ app/components/Menu/Switch.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface SwitchProps {
+  isChecked: boolean;
+  onChange: (checked: boolean) => void;
+}
+
+const Switch: React.FC<SwitchProps> = ({ isChecked, onChange }) => {
+  return (
+    <label className="relative inline-flex items-center cursor-pointer">
+      <input
+        type="checkbox"
+        className="sr-only"
+        checked={isChecked}
+        onChange={(e) => onChange(e.target.checked)}
+      />
+      <div className={`w-11 h-6 rounded-full ${isChecked ? `bg-[#304d6d] dark:bg-blue-600` : `bg-gray-400`} relative transition-colors`}>
+        <span
+          className={`absolute top-0.5 left-0.5 w-5 h-5 bg-white border border-gray-300 rounded-full transition-transform ${isChecked ? 'transform translate-x-5' : ''}`}
+        ></span>
+      </div>
+    </label>
+  );
+};
+
+export default Switch;
