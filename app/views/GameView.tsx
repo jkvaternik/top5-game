a4932579bbbaa975908ca82aead6cdb580f66aff
Romey Sklar <romey@hey.com>
2024-08-26 22:25:17 -0400

    fix

--- app/views/GameView.tsx
+++ app/views/GameView.tsx
@@ -7,7 +7,7 @@ import Header from './Header';
 import InputComponent from '../components/InputComponent';
 import RankList from '../components/RankList/RankList';
 
-import useDailyPuzzle, { Answer, Puzzle } from '../hooks/useDailyPuzzle';
+import useDailyPuzzle, { Answer, Puzzle, RankedAnswer } from '../hooks/useDailyPuzzle';
 import { getCurrentLocalDateAsString, getScore } from '../utils';
 import { useGameState } from '../hooks/useGameState';
 import ArchiveModal from '../components/ModalComponent/Modals/ArchiveModal';
@@ -80,7 +80,7 @@ export default function GameView({ setShowInstructionsModal }: GameViewProps) {
         </section>
         <br></br>
         <section className="flex flex-col gap-4">
-          <RankList guesses={guesses} answers={puzzle.answers} options={puzzle.optionsKey ? undefined : puzzle.options as Answer[]} isGameOver={gameOver} />
+          <RankList guesses={guesses} answers={puzzle.answers} options={puzzle.optionsKey ? undefined : puzzle.options as RankedAnswer[]} isGameOver={gameOver} />
         </section>
       </>
       }
