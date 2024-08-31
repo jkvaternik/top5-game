c58b05853593e2b61ab73b3e888fed9a840b2b28
Romey Sklar <romey@hey.com>
2024-08-31 15:45:10 -0400

    no instructions on archive

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import React, { Suspense } from "react";
 import { useEffect, useState } from "react";
 import 'react-toastify/dist/ReactToastify.css';
+import { useSearchParams } from "next/navigation";
 
 import { InstructionsModal } from "./components/ModalComponent/Modals/InstructionsModal";
 import GameView from "./views/GameView";
@@ -12,17 +13,20 @@ import { isNewVisitor } from "./utils";
 export default function Home() {
   const [showInstructionsModal, setShowInstructionsModal] = useState(false);
 
+  const searchParams = useSearchParams()
+  const isArchiveMode = searchParams.has('date')
+
   useEffect(() => {
     if (typeof window === 'undefined') return;
 
     const newUser = isNewVisitor();
 
-    if (newUser) {
+    if (newUser && !isArchiveMode) {
       setShowInstructionsModal(true);
     }
 
     localStorage.setItem('lastVisit', JSON.stringify(new Date().toLocaleString()));
-  }, []);
+  }, [isArchiveMode]);
 
   return (
     <main style={{ margin: '4vh auto' }} className="w-10/12 sm:w-8/12 md:w-1/2">
